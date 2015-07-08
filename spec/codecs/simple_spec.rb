39eba84ce5da1ae885454d35e795d25aa60a1766
Rigoberto Vazquez <rigoberto.vazquez@sanmina.com>
2015-07-08 11:53:47 -0500

    first Commit

---
+++ spec/codecs/simple_spec.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+require "logstash/codecs/simple"
+require "logstash/event"
+require "insist"
+
+describe LogStash::Codecs::Multiline do
+  context "#decode" do
+    it "should be able to handle multiline events with additional lines space-indented" do
+
+    end
+
+    context "using default UTF-8 charset" do
+
+    end
+
+    context "with invalid non UTF-8 source encoding" do
+
+
+    end
+  end
+
+  context "with non closed multiline events" do
+
+
+  end
+end
